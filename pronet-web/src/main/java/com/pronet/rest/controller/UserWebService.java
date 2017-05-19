fa763bf832c0fd962ac322822cef8e48111e6edf
prithwin <getprith@gmail.com>
2017-05-20 00:25:11 +0530

    adding the user controller

--- pronet-web/src/main/java/com/pronet/rest/controller/UserWebService.java
+++ pronet-web/src/main/java/com/pronet/rest/controller/UserWebService.java
@@ -3,7 +3,9 @@ package com.pronet.rest.controller;
 import com.pronet.service.UserService;
 import org.pronet.common.vo.UserVo;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.util.UUID;
@@ -23,5 +25,21 @@ public class UserWebService {
             {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
     public @ResponseBody UserVo getUserDetails(@PathVariable(value = "userId") int userId){
         return userService.getUser(userId);
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/mirror/{userId}",method = RequestMethod.POST,produces = {MediaType.APPLICATION_XML_VALUE}
+            ,consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public @ResponseBody UserVo mirror(@PathVariable int userId,@RequestPart MultipartFile multipartFile){
+        System.out.print("");
+        return null;
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "/mirror2/{userId}",method = RequestMethod.POST,produces = {MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_XML_VALUE})
+    public @ResponseBody UserVo mirror2(@PathVariable int userId,@RequestBody @Validated UserVo chot){
+        System.out.print("");
+        return null;
     }
 }
